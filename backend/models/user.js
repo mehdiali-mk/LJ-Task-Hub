e04ba302971d9fc03159ea72a93dea537cab9cba
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2025-09-14 12:01:34 +0530

    Initial Commin.

---
+++ backend/models/user.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: { type: String, required: true, select: false },
+    name: { type: String, required: true, trim: true },
+    profilePicture: { type: String },
+    lastLogin: { type: Date },
+  },
+  { timestamps: true }
+);
+const User = mongoose.model("User", userSchema);
+
+export default User;
